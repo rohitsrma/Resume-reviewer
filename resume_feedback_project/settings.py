02a1c6ef0d0b0c10a9aabe9a9637060232e2134a
Rohit Sharma <111359305+rohitsrma@users.noreply.github.com>
2024-03-13 20:12:52 +0530

    Update settings.py

--- resume_feedback_project/settings.py
+++ resume_feedback_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^91$8f2(prj#ms0gcsn1)pvb010z5#9^4p*2s=k&udg@1277dg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
